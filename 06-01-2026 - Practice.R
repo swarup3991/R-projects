5f6fe49fa3fd72b4f3401ab6274a34f525a87567
Swarup Das <swarupdas3991@gmail.com>
2026-01-29 23:59:29 +0530

    Add files via upload

---
+++ 06-01-2026 - Practice.R
@@ -0,0 +1,15 @@
+Serial <- c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
+str(Serial)
+Name <- c('Max', 'Troy', 'Tony', 'Peter', 'Ray', 'Julie', 'Daisy', 'Sam', 'Nancy', 'Mona')
+str(Name)
+Age <- c(19L, 20L, 22L, 18L, 25L, 21L, 23L, 22L, 23L, 24L)
+str(Age)
+Marks <- c(78, 73, 77, 81, 70, 89, 93, 75, 80, 91)
+str(Marks)
+Status <- c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE)
+str(Status)
+
+data <- data.frame(Serial, Name, Age, Marks, Status)
+
+#view the data
+data
